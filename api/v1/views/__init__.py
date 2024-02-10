048e1c0d37716d40683a4254bb9c7ffbeab59b96
leakagegh <thehealer184@gmail.com>
2024-02-10 18:17:11 +0000

    2...

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,16 +1,9 @@
 #!/usr/bin/python3
-"""
-Contains the blueprint for the API
-"""
-from flask import Blueprint
 
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+from flask import jsonify
+from api.v1.views import app_views
 
-from api.v1.views.index import *
-from api.v1.views.states import *
-from api.v1.views.cities import *
-from api.v1.views.amenities import *
-from api.v1.views.users import *
-from api.v1.views.places import *
-from api.v1.views.places_reviews import *
-from api.v1.views.places_amenities import *
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
+def get_status():
+    """Returns the status of the API"""
+    return jsonify({"status": "OK"})
